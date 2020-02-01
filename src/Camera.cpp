8a75a56ce215b31db4794ca793048c87b51b53b7
jxarco <alexroco.30@gmail.com>
2020-02-01 04:42:37 +0100

    camera updates

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,4 +1,5 @@
 #include "Camera.hpp"
+#include "Math.hpp"
 #include "Gfx.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,41 +8,69 @@
 Camera::Camera() :
     mPosition(Vector2(0, 0))
 {
+    Default();
 }
 
-/* 1 player tracking. */
-void Camera::Update(const Vector2& p1)
+Camera::Camera(const Vector2& initialPos) :
+    mPosition(initialPos)
 {
-    mPosition = p1;
+    Default();
 }
 
-/* 2 player tracking. */
-void Camera::Update(const Vector2& p1, const Vector2& p2)
+void Camera::Default()
 {
-    mPosition.X.value = abs(p1.X.value - p2.X.value) / 2;
-    mPosition.Y.value = abs(p1.Y.value - p2.Y.value) / 2;
-
-    if (p1.X.value <= p2.X.value)
-    {
-        mPosition.X.value = p1.X.value + mPosition.X.value;
-    }
-    else
-    {
-        mPosition.X.value = p2.X.value + mPosition.X.value;
-    }
-
-    if (p1.Y.value <= p2.Y.value)
-    {
-        mPosition.Y.value = p1.Y.value + mPosition.Y.value;
-    }
-    else
-    {
-        mPosition.Y.value = p2.Y.value + mPosition.Y.value;
-    }
+    smoothing = true;
+    smooth_value = 0.9;
+    
+    shake_elapsed = 0;
+	shake_length = 0;
+	shake_amount = 0;
 }
 
 void Camera::getPosition(short& x, short& y) const
 {
     x += (X_SCREEN_RESOLUTION >> 1) - fix16_to_int(mPosition.X.value);
     y += (Y_SCREEN_RESOLUTION >> 1) - fix16_to_int(mPosition.Y.value);
+}
+
+void Camera::Update()
+{
+    // Follow always the player
+    LookAt( Vector2(0, 0)/*  */ );
+
+    if (shake_length > 0)
+	{
+		int shakeX = rand() * shake_amount * 2 - shake_amount;
+		int shakeY = rand() * shake_amount * 2 - shake_amount;
+
+		mPosition.X += shakeX;
+		mPosition.Y += shakeY;
+
+		shake_length--;
+	}
+}
+
+void Camera::Shake(unsigned int amount, unsigned int length)
+{
+    // Assign shaking values
+    shake_amount = amount;
+	shake_length = length;
+}
+
+void Camera::LookAt(const Vector2& target, bool smooth)
+{
+	int gameMapSize = 0;//World::instance->map.width;
+
+    // Poner esto fuera y poder cogerlo desde aquí
+	int bufferX = 368;
+    int bufferY = 240;
+
+	int marginX = bufferX / 2;
+    int marginY = bufferY / 2;
+
+	int targetx = clamp(target.X.value, marginX, gameMapSize - marginX) - bufferX / 2;
+	int targety = clamp(target.Y.value, marginY, gameMapSize - marginY) - bufferY / 2;
+
+	mPosition.X = targetx;//smooth ? lerp(targetx, mPosition.X.value, smooth_value) : targetx;
+	mPosition.Y = targety;//smooth ? lerp(targety, mPosition.Y.value, smooth_value) : targety;
 }
--- src/Camera.hpp
+++ src/Camera.hpp
@@ -2,15 +2,34 @@
 
 #include "Vector2.hpp"
 
+// class Player;
+
+class Player;
+
 class Camera
 {
 public:
+    
     Camera();
-    void Update(const Vector2& p1);
-    void Update(const Vector2& p1, const Vector2& p2);
+    Camera(const Vector2& initialPos);
+
+    void Default();
+    
     void getPosition(short& x, short& y) const;
 
+    void Update();
+    void Shake(unsigned int amount, unsigned int length);
+    void LookAt(const Vector2& position, bool smoothing = false);
+
 private:
+    
     Vector2 mPosition;
+
+    bool smoothing;
+    float smooth_value;
+
+    unsigned int shake_elapsed;
+	unsigned int shake_length;
+	unsigned int shake_amount;
 
 };
