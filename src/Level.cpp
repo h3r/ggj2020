087a86a6b7582b13274e8b295bd272a4b2b2f5a0
Mik-C <miquelclark@hotmail.com>
2020-02-01 05:14:54 +0100

    Level Load, GetDimensions. Render WIP

--- src/Level.cpp
+++ src/Level.cpp
@@ -12,6 +12,7 @@
 
 #include "Level.hpp"
 #include "IO.h"
+#include "Gfx.h"
 
 /* *************************************
  * Defines
@@ -54,24 +55,39 @@ void Level::Update(GlobalData& data){
 }
 
 void Level::Render(const Camera& cam){
-
+//	short x, y;
+//	cam.getPosition(&x, &y);
+//
+//
+//
+//	uint8_t x_tile = x / TILE_SIZE;
+//	uint8_t y_tile = y / TILE_SIZE;
+//
+//	uint8_t horizontal_tiles = 1 + X_SCREEN_RESOLUTION / TILE_SIZE;
+//	uint8_t vertical_tiles = 1 + Y_SCREEN_RESOLUTION / TILE_SIZE;
+//
+//	for(uint8_t i=0; i<horizontal_tiles; i++){
+//		for(uint8_t j=0; j<vertical_tiles; j++){
+//
+//		}
+//	}
 }
 
-bool Level::load(const char* filename){
+bool Level::Load(const char* file_name){
 	/* File size in bytes. Modified by IOLoadFile(). */
-    size_t eSize;
-    size_t levelSize = MAX_LEVEL_SIZE_X*MAX_LEVEL_SIZE_Y;
+    size_t e_size;
+    size_t level_size = MAX_LEVEL_SIZE_X*MAX_LEVEL_SIZE_Y;
 
     /* Get buffer address where file data is contained. */
-    const uint8_t *const buffer = IOLoadFile(filename, &eSize);
+    const uint8_t *const buffer = IOLoadFile(file_name, &e_size);
 
-    if(!buffer || eSize == IO_INVALID_FILE_SIZE || eSize != (levelSize-2))
+    if(!buffer || e_size == IO_INVALID_FILE_SIZE || e_size != (level_size+2))
     	return false;
 
     width = buffer[0];
     height = buffer[1];
 
-    for (size_t i=0; i<levelSize; i++) {
+    for (size_t i=0; i<level_size; i++) {
     	if(buffer[i+2]<LEVEL_MAX_TILES) return false;
     	tiles[i] = buffer[i+2];
     }
@@ -79,6 +95,22 @@ bool Level::load(const char* filename){
     return true;
 }
 
+bool Level::LoadAssets(){
+	return GfxSpriteFromFile("DATA\\SPRITES\\tileset.bmp", &tile_set);
+}
+
+void Level::TestLevel(){
+	width = 16;
+	height = 16;
+	for(size_t i=0; i<width*height; i++){
+		tiles[i] = i%2;
+	}
+}
+
+void Level::GetDimensions(short& w, short& h) const{
+	w = width * TILE_SIZE;
+	h = height * TILE_SIZE;
+}
 
 /*******************************************************************//**
 *
--- src/Level.hpp
+++ src/Level.hpp
@@ -1,23 +1,33 @@
 
 #pragma once
 
+#include <psxgpu.h>
 #include <stdint.h>
 
 class GlobalData;
 class Camera;
 
-#define MAX_LEVEL_SIZE_X 16
-#define MAX_LEVEL_SIZE_Y 16
+enum{
+	MAX_LEVEL_SIZE_X = 32,
+	MAX_LEVEL_SIZE_Y = 16,
+	TILE_SIZE = 16
+};
 
 class Level{
 public:
 	Level();
 	void Update(GlobalData& data);
 	void Render(const Camera& cam);
-	bool load(const char* filename);
+	bool Load(const char* file_name);
+	bool LoadAssets();
+	void TestLevel();
+
+	void GetDimensions(short& w, short& h) const;
 
 private:
 	uint8_t width;
 	uint8_t height;
 	uint8_t tiles[MAX_LEVEL_SIZE_X*MAX_LEVEL_SIZE_Y];
+
+	GsSprite tile_set;
 };
